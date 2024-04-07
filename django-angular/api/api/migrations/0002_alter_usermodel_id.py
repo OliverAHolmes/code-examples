3ba406d9a14e79b2145f440a52342c831dffad17
Oliver Holmes <oliver.albert.holmes@gmail.com>
2024-04-07 15:30:47 +0930

    Move the api directory

--- django-angular/api/api/migrations/0002_alter_usermodel_id.py
+++ django-angular/api/api/migrations/0002_alter_usermodel_id.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("apiexample", "0001_initial"),
+        ("api", "0001_initial"),
     ]
 
     operations = [
